edf3192538a0eef9fa8af38d4c8d50bb20869e10
liliana <lilunia32@wp.pl>
2016-03-06 12:03:05 +0100

    06.03.2016r.

---
+++ src/content.js
@@ -0,0 +1,4 @@
+/**
+ * Created by liliana on 06.03.16.
+ */
+module.exports = "It works from lala content.js.";
---
+++ src/entry.js
@@ -0,0 +1,4 @@
+document.write(require("./content.js"));
+var $ = require('jquery');
+
+$('body').append($('<div id="app">'));
